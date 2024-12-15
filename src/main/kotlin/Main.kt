47143b242ebeca90854bfa24f590f812a738df38
Erik S. <gitarist.erik@gmail.com>
2024-12-15 15:16:32 +0200

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main() {
+    listOf<BaseSolution>(
+//        Task1(),
+    ).forEach { it.run() }
+}
+
+abstract class BaseSolution {
+    val solutionName = this::class.simpleName
+
+    protected abstract fun runSolution()
+
+    fun run() {
+        println("--------------------------")
+        println("Running solution: $solutionName")
+        runSolution()
+    }
+}
